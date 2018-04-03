90467416865d1a4b3e8fea589bc1e673ad3f540f
liuhong-summer <1150487420@qq.com>
2018-04-03 18:08:35 +0800

    安装element-ui

--- project/src/main.js
+++ project/src/main.js
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone); has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 // import router from './router'
 
-Vue.config.debug = true
+Vue.config.debug = true;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: {App},
   template: '<App/>'
-})
+});
