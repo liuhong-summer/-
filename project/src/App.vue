35554310a0fbe45c8147446a0009569586a269ab
liuhong <1150487420@qq.com>
2018-04-03 22:17:46 +0800

    首页制作

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,11 +1,54 @@
 <template>
   <div >
     <div class="contain">
-      <span>位置</span>
-      <span>文科</span>
-      <span>分数</span>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <i class="el-icon-location color-blue font-size-18"></i><span>{{position}}</span>
+        </el-col>
+        <el-col :span="8">{{subject}}</el-col>
+        <el-col :span="8">{{score}}</el-col>
+      </el-row>
     </div>
   </div>
 </template>
 <script>
+export default {
+  data () {
+    return {
+      position: '四川',
+      subject: '文科',
+      score: '586'
+    };
+  }};
 </script>
+<style>
+  .el-row {
+    margin-bottom: 20px;
+  }
+  .el-row:last-child{
+    margin-bottom: 0;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+  .contain .el-row{
+    text-align: center;
+  }
+</style>
--- project/src/main.js
+++ project/src/main.js
@@ -2,13 +2,17 @@
 // (runtime-only or standalone); has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import Element from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '../static/css/common.css';
 // import router from './router'
 
 Vue.config.debug = true;
-
+Vue.use(Element);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: {App},
-  template: '<App/>'
+  template: '<App/>',
+  render: h => h(App)
 });
